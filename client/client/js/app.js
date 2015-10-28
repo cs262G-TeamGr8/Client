c3efbf1f6be3e39b3c3a1eb1b00c039ec2a3fa37
Jesse Bloomster <jabloomster@gmail.com>
2015-10-27 23:58:34 -0400

    Added necessary angularjs files. Added app.js file which is the main angular file

---
+++ client/client/js/app.js
@@ -0,0 +1,14 @@
+﻿/*-----------------------------------------------------------------------------------------------
+
+    This app references the intramurals database, which has yet to be constructed. 
+    Angular controllers are used to control the viewing of pages and the data binding
+    ind order to display specific statistics and data. As of 10/27/2015, both the client and
+    the database are both underconstruction. There is no Web API, just a database on a server
+    for data gathering.
+            
+-------------------------------------------------------------------------------------------------*/
+
+var app = angular.module("CalvinIntramurals", ["ui.bootstrap", "ngRoute"])
+    .config(['$controllerProvider', function ($controllerProvider) {
+        $controllerProvider.allowGlobals();
+    }]);
