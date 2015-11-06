150ccbcbb110674fceaee64c710b220170e2c956
Jesse Bloomster <jabloomster@gmail.com>
2015-11-05 22:09:40 -0500

    Added forgot password to login modal. Started developing sign up modal within login.

---
+++ client/client/modals/signup/signupController.js
@@ -0,0 +1,18 @@
+﻿/*-------------------------------------------------------------------
+
+    The SignUpCtrl provides the view for a user to create
+    an intramural account in order to join/create a team
+    and access other logged in features of the webservice.
+
+----------------------------------------------------------------------*/
+
+var app = angular.module("CalvinIntramuralsApp")
+
+
+app.controller('SignUpCtrl', ['$rootScope', '$scope', '$http', '$uibModalInstance', '$uibModal', function ($rootScope, $scope, $http, $uibModalInstance, $uibModal) {
+
+    $scope.cancel = function () {
+        $uibModalInstance.dismiss("cancel");
+    }
+
+}]);
