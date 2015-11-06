d9c3a8589a5fb98ca4259c776c41596ef367e779
jabloomster <jabloomster@gmail.com>
2015-11-05 22:28:42 -0500

    Add teamController.js
    Adding controller file because original file was not included in initial commit for some reason.

---
+++ client/client/pages/team/teamController.js
@@ -0,0 +1,34 @@
+/*---------------------------------------------------------------------------
+
+        The TeamCtrl is the controller for the myTeams option
+        in the menu bar. Based on the parameter sent in by $location, 
+        this controller will display the current info regarding a specific
+        team for which the logged in user is a part of.
+
+-----------------------------------------------------------------------------*/
+
+
+
+var app = angular.module("CalvinIntramuralsApp")
+
+app.controller('TeamCtrl', ['$rootScope', '$scope', '$http', '$routeParams', '$uibModal', function ($rootScope, $scope, $http, $routeParams, $uibModal) {
+
+    $scope.message = $routeParams.name;
+
+    $scope.openLoginModal = function () {
+        var modalInstance = $uibModal.open({
+            templateUrl: 'modals/login/login.html',
+            controller: 'LoginCtrl',
+            animation: false,
+            windowClass: 'app-modal-window'
+        });
+
+        modalInstance.result.then(function () {
+            //runs in here if something passed back in close()
+        }, function () {
+            //runs if modal is dismissed
+            console.log("Results dismissed at: " + new Date());
+        });
+    }
+
+}]);
