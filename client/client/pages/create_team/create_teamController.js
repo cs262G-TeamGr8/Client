4e71bd6189ad234c2a1799c7fefce580feb97c6f
bdv9 <bdv9@students.calvin.edu>
2015-12-15 16:48:05 -0500

    comments

--- client/client/pages/create_team/create_teamController.js
+++ client/client/pages/create_team/create_teamController.js
@@ -10,38 +10,49 @@
 
 var app = angular.module("CalvinIntramuralsApp")
 
-app.controller('CreateTeamCtrl', ['$rootScope', '$scope', '$http', '$location', function ($rootScope, $scope, $http, $location) {
+app.controller('CreateTeamCtrl', ['$rootScope', '$scope', '$http', '$location',
+    function ($rootScope, $scope, $http, $location) {
 
+    // automatically closes menu bar
     if ($(window).width() <= (767) && $("body").hasClass("sidebar-open")) {
         $("body").removeClass('sidebar-open');
     }
 
+    // GET: the list of leagues
     $http.get($rootScope.apiScope + 'league/leagues/late fall').success(function (result) {
         leagueList = JSON.parse(result);
-
         for (var i = 0; i < leagueList.length; i++) {
             var leagueName = leagueList[i]["name"];
 
             // use JQuery to add leagues to select tag
-            $("#leagueSelect").append('<option value="' + leagueName + '">' + leagueName + '</option>')
+            $("#leagueSelect").append('<option value="' + leagueName + '">' + leagueName +
+                '</option>')
         }
     })
     .error(function (data, status, headers, config) {
         $log.info("League retrieval failure");
     });
-
+    
+    // gets data from user regarding the team to be created
     $scope.createTeam = function () {
         var team = {
             League: $("#leagueSelect").val(),
             Name: $("#teamName").val(),
+
+            // sets the Email to be the email of the logged in user
             Email: $rootScope.loggedIn.email
         }
 
         var url = "team/new";
         url = $rootScope.apiScope + url;
+
+        // POST: creates the team
         $http.post(url, JSON.stringify(team)).success(function (result) {
             
-            var url2 = 'user/join?userId=' + $rootScope.loggedIn.id + '&teamName=' + $("#teamName").val();
+            var url2 = 'user/join?userId=' + $rootScope.loggedIn.id +
+                '&teamName=' + $("#teamName").val();
+
+            // POST: if create is successful joins the team
             $http.post($rootScope.apiScope + url2).success(function (result) {
  
             $rootScope.newTeam = $("#teamName").val();
--- client/client/pages/join_team/join_teamController.js
+++ client/client/pages/join_team/join_teamController.js
@@ -10,38 +10,43 @@
 
 var app = angular.module("CalvinIntramuralsApp")
 
-app.controller('JoinTeamCtrl', ['$rootScope', '$scope', '$http', '$log', '$location', function ($rootScope, $scope, $http, $log, $location) {
+app.controller('JoinTeamCtrl', ['$rootScope', '$scope', '$http', '$log', '$location',
+    function ($rootScope, $scope, $http, $log, $location) {
 
     $scope.selectedTeam = '____________';
 
+    // automatically closes menu bar
     if ($(window).width() <= (767) && $("body").hasClass("sidebar-open")) {
         $("body").removeClass('sidebar-open');
     }
 
+    // GET: the list of leagues
     $http.get($rootScope.apiScope + 'league/leagues/late fall').success(function (result) {
         leagueList = JSON.parse(result);
-
         for (var i = 0; i < leagueList.length; i++) {
             var leagueName = leagueList[i]["name"];
 
             // use JQuery to add leagues to select tag
-            $("#leagueSelect").append('<option value="' + leagueName + '">' + leagueName + '</option>')
+            $("#leagueSelect").append('<option value="' + leagueName + '">' + leagueName +
+                '</option>')
         }
     })
     .error(function (data, status, headers, config) {
         $log.info("League retrieval failure");
     });
-
+    
+    // if league is changed refresh list of teams
     $('#leagueSelect').on('change', function (e) {
         var optionSelected = $("option:selected", this);
         var league = this.value;
 
         $('#teamSelect').empty();
-        $('#teamSelect').append('<option value="" selected="selected" disabled="disabled">Select a team</option>');
+        $('#teamSelect').append('<option value="" selected="selected" disabled="disabled">' +
+            'Select a team</option>');
 
+        // GET: gets teams in selected league
         $http.get($rootScope.apiScope + 'league/teams/' + league).success(function (result) {
             var teamList = JSON.parse(result);
-
             for (var i = 0; i < teamList.length; i++) {
                 $('#teamSelect')
                     .append($("<option></option>")
@@ -52,10 +57,12 @@ app.controller('JoinTeamCtrl', ['$rootScope', '$scope', '$http', '$log', '$locat
 
     });
 
+    // joins the selected team
     $scope.joinTeam = function () {
-
         var team = $('#teamSelect').val();
         var url = 'user/join?userId=' + $rootScope.loggedIn.id + '&teamName=' + team;
+
+        // POST: joins the selected team
         $http.post($rootScope.apiScope + url).success(function (result) {
 
             $rootScope.newTeam = $("#teamSelect").val();
